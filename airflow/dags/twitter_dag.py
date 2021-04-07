6231eda5aaafd2cc29c189f2c75bcdec0edc4b39
Rafael Bottega <rbottega@worldremit.com>
2021-04-07 21:40:27 +0100

    Aula 5

--- airflow/dags/twitter_dag.py
+++ airflow/dags/twitter_dag.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from os.path import join
+
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.models import DAG
 from airflow.operators.alura import TwitterOperator
 
@@ -13,4 +15,23 @@ with DAG(dag_id="twitter_dag", start_date=datetime.now()) as dag:
             "extract_date={{ ds }}",
             "AluraOnline_{{ ds_nodash }}.json"
         )
+    )
+
+    twitter_transform = SparkSubmitOperator(
+        task_id="transform_twitter_aluraonline",
+        application=(
+            "/Users/rbottega/Documents/alura/"
+            "datapipeline/spark/transformation.py"
+        ),
+        name="twitter_transformation",
+        application_args=[
+            "--src",
+            "/Users/rbottega/Documents/alura/datapipeline/datalake/"
+            "bronze/twitter_aluraonline/extract_date=2021-03-04",
+            "--dest",
+            "/Users/rbottega/Documents/alura/datapipeline/datalake/"
+            "silver/twitter_aluraonline",
+            "--process-date",
+            "{{ ds }}",
+        ]
     )
--- spark/transformation.py
+++ spark/transformation.py
@@ -1,13 +1,62 @@
+import argparse
+from os.path import join
+
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+
+
+def get_tweets_data(df):
+    return df\
+        .select(
+            f.explode("data").alias("tweets")
+        ).select(
+            "tweets.author_id",
+            "tweets.conversation_id",
+            "tweets.created_at",
+            "tweets.id",
+            "tweets.in_reply_to_user_id",
+            "tweets.public_metrics.*",
+            "tweets.text"
+        )
+
+
+def get_users_data(df):
+    return df\
+        .select(
+            f.explode("includes.users").alias("users")
+        ).select(
+            "users.*"
+        )
+
+
+def export_json(df, dest):
+    df.coalesce(1).write.mode("overwrite").json(dest)
+
+
+def twitter_transform(spark, src, dest, process_date):
+    df = spark.read.json(src)
+
+    tweet_df = get_tweets_data(df)
+    user_df = get_users_data(df)
+
+    table_dest = join(dest, "{table_name}", f"process_date={process_date}")
+
+    export_json(tweet_df, table_dest.format(table_name="tweet"))
+    export_json(user_df, table_dest.format(table_name="user"))
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Spark Twitter Transformation"
+    )
+    parser.add_argument("--src", required=True)
+    parser.add_argument("--dest", required=True)
+    parser.add_argument("--process-date", required=True)
+    args = parser.parse_args()
+
     spark = SparkSession\
         .builder\
         .appName("twitter_transformation")\
         .getOrCreate()
-    
-    df = spark.read.json(
-        "/Users/rbottega/Documents/alura/datapipeline/datalake/twitter_aluraonline"
-    )
-    df.printSchema()
-    df.show()
+
+    twitter_transform(spark, args.src, args.dest, args.process_date)
